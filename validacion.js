8da9fe569d77eeae7fa87fe497c27ece68befd6d
Dexametasona <machicadogomezalexander@gmail.com>
2023-07-11 15:04:32 -0500

    adding footer and form styles

--- validacion.js
+++ validacion.js
@@ -1,1 +1,15 @@
-//Haz tú validación en javascript acá
+function labelIn(id){
+  const label=document.querySelector(`[data-label-${id}]`);
+  const input=document.querySelector(`[data-input-${id}]`)
+
+  label.classList.add('labelIn')
+  
+}
+function labelOut(id){
+  const label=document.querySelector(`[data-label-${id}]`);
+  const input=document.querySelector(`[data-input-${id}]`)
+
+  if(input.value=='' || input.value==null){
+    label.classList.toggle('labelIn')
+  }
+}
